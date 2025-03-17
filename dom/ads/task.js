87ee36124868500d76b76e27e3f52d4544b21834
Dima Neto <kuznetsovdv2013@gmail.com>
2025-03-17 08:26:57 +0300

    ads задание со звездочкой

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,6 +1,30 @@
 const rotators = document.querySelectorAll('.rotator__case');
 
 let currentIndex = 0;
+
+function updateRotator () {
+    rotators[currentIndex].classList.remove('rotator__case_active');
+
+    currentIndex = (currentIndex + 1) % rotators.length;
+    const nextRotator = rotators[currentIndex];
+
+    const color = nextRotator.getAttribute('data-color');
+
+    nextRotator.style.color = color;
+    nextRotator.classList.add('rotator__case_active');
+
+    const speed = parseInt(nextRotator.getAttribute('data-speed'), 10);
+
+    setTimeout (updateRotator, speed);
+};
+
+updateRotator()
+
+
+
+/*const rotators = document.querySelectorAll('.rotator__case');
+
+let currentIndex = 0;
 const speed = 1000;
 
 rotators.forEach ((rotator, index) => {
@@ -14,4 +38,4 @@ setInterval (() => {
     rotators[currentIndex].classList.remove('rotator__case_active');
     currentIndex = (currentIndex + 1) % rotators.length;
     rotators[currentIndex].classList.add('rotator__case_active');
-}, speed);
+}, speed);*/
