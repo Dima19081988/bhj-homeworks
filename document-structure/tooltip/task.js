ba9c96f0ad993cf86d400f3da7a1693494d0e091
Dima Neto <kuznetsovdv2013@gmail.com>
2025-03-21 17:05:54 +0300

    внесены коррекции и координаты

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,22 +3,26 @@ const helpTitles = document.querySelectorAll('.has-tooltip');
 function openToolTip(event) {
     event.preventDefault();
 
-
     const activeToolTip = document.querySelector('.tooltip_active');
+    const title = this.getAttribute('title');
     if (activeToolTip) {
-        activeToolTip.classList.remove('tooltip_active');
-        activeToolTip.remove();
-    };
+        if (activeToolTip.textContent === title) {
+            activeToolTip.classList.remove('tooltip_active');
+            activeToolTip.remove();
+            return; 
+        } else {
+            activeToolTip.classList.remove('tooltip_active');
+            activeToolTip.remove();
+        }
+    }
 
     const toolTip = document.createElement('div');
     toolTip.className = 'tooltip';
-
-
-    const title = this.getAttribute('title');
     toolTip.textContent = title;
-
-
-    this.parentNode.insertBefore(toolTip, this.nextSibling);
+    document.body.appendChild(toolTip);
+    const rect = this.getBoundingClientRect();  
+    toolTip.style.left = `${rect.left}px`;
+    toolTip.style.top = `${rect.bottom}px`;
     toolTip.classList.add('tooltip_active');
 };
 
